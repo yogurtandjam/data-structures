38673d09e809658f9daf97e24fea75c23f6605c9
danieljhegeman <student@hackreactor.com>
2018-04-26 15:02:09 -0700

    finished implementing tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,34 @@
 var Tree = function(value) {
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.index = 0;
+  newTree.children = {};  // fix me
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+   var child = Tree(value);
+   this.children[this.index] = child;
+   this.index++;
 };
 
 treeMethods.contains = function(target) {
+  for (var i in this.children){
+    if(this.children[i].value === target){
+      return true;
+    }
+    if(typeof this.children[i] === 'object'){
+      if(this.children[i].contains(target)){
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
