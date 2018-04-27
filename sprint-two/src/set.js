89713e86e5f9acd3129188f77e0462b7eff47492
danieljhegeman <student@hackreactor.com>
2018-04-26 17:58:23 -0700

    implemented set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
+  set.index = 1;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[this.index] = item;
+  this.index++;
 };
 
 setPrototype.contains = function(item) {
+  for (let i = 0; i < this.index; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (let i = 0; i < this.index; i++) {
+    if (this.storage[i] === item) {
+      delete this.storage[i];
+      this.index --;
+    }
+  }
 };
 
 /*
